6f8beb305031bc0a4dfc67f567681f670082bbb7
True Goodwill <true@truegoodwill.com>
2022-02-24 06:58:22 -0500

    Added RetryingSubscription and CatchupBroadcastHub

---
+++ src/FFT.Subscriptions/CatchupBroadcastHub.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) True Goodwill. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace FFT.Subscriptions
+{
+  using System;
+  using System.Collections.Generic;
+
+  /// <summary>
+  /// This implementation of <see cref="IBroadcastHub"/> keeps a record of the last-sent message
+  /// and immediately sends it to new subscribers, effectively catching them up on the latest
+  /// information when they join.
+  /// </summary>
+  public sealed class CatchupBroadcastHub : IBroadcastHub
+  {
+    private readonly List<IWritable> _subscribers = new();
+
+    private object? _lastMessage;
+
+    /// <inheritdoc/>
+    public int SubscriberCount => _subscribers.Count;
+
+    /// <inheritdoc/>
+    public void AddSubscriber(IWritable subscriber)
+    {
+      _subscribers.Add(subscriber);
+      if (_lastMessage is not null)
+        subscriber.Write(_lastMessage);
+    } 
+
+    /// <inheritdoc/>
+    public void RemoveSubscriber(IWritable subscriber)
+      => _subscribers.Remove(subscriber);
+
+    /// <inheritdoc/>
+    public void Handle(object message)
+    {
+      foreach (var subscriber in _subscribers)
+        subscriber.Write(message);
+      _lastMessage = message;
+    }
+
+    /// <inheritdoc/>
+    public void Complete(Exception? error)
+    {
+      foreach (var subscriber in _subscribers)
+        subscriber.Complete(error);
+    }
+  }
+}
---
+++ src/FFT.Subscriptions/RetryingSubscription.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) True Goodwill. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace FFT.Subscriptions
+{
+  using System;
+  using System.Threading.Channels;
+  using System.Threading.Tasks;
+  using FFT.Disposables;
+  using FFT.IgnoreTasks;
+
+  public sealed class RetryingSubscription : DisposeBase, ISubscription
+  {
+    private readonly Channel<object> _channel = Channel.CreateBounded<object>(1);
+    private readonly Func<ISubscription> _getSubscription;
+
+    public RetryingSubscription(Func<ISubscription> getSubscription)
+    {
+      _getSubscription = getSubscription;
+      Task.Run(Work).Ignore();
+    }
+
+    public ChannelReader<object> Reader => _channel.Reader;
+
+    private async Task Work()
+    {
+      while (true)
+      {
+        try
+        {
+          using var subscription = _getSubscription();
+          await foreach (var message in subscription.Reader.ReadAllAsync(DisposedToken))
+          {
+            await _channel.Writer.WriteAsync(message, DisposedToken);
+          }
+
+          await _channel.Writer.WriteAsync(new SubscriptionInterruptionMessage(null), DisposedToken);
+          await Task.Delay(1000, DisposedToken);
+        }
+        catch (Exception x)
+        {
+          // The inner subscription reader can throw
+          // OperationCanceledException when it is disposed due to feed
+          // issues. Therefore we can't use a special
+          // OperationCanceledException check here and expect to use that to
+          // know when we are being disposed. Must use this property instead.
+          if (IsDisposeStarted)
+          {
+            _channel.Writer.Complete();
+            return;
+          }
+          else
+          {
+            await _channel.Writer.WriteAsync(new SubscriptionInterruptionMessage(x), DisposedToken);
+            await Task.Delay(1000, DisposedToken);
+          }
+        }
+      }
+    }
+  }
+}
